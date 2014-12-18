13f951829bd12c865adf7087ea16717835051588
ajochope <ocalles@gmail.com>
2014-12-18 14:51:09 +0100

    Tutorial Objective-C Multithread
    Grab images from a web page and show  a table
    
    http://www.raywenderlich.com/4295/multithreading-and-grand-central-dispa
    tch-on-ios-for-beginners-tutorial

---
+++ ImageGrabber/ImageGrabberAppDelegate.m
@@ -0,0 +1,84 @@
+//
+//  ImageGrabberAppDelegate.m
+//  ImageGrabber
+//
+//  Created by Ray Wenderlich on 7/3/11.
+//  Copyright 2011 Ray Wenderlich. All rights reserved.
+//
+
+#import "ImageGrabberAppDelegate.h"
+
+@implementation ImageGrabberAppDelegate
+
+/*
+ 
+ @synthesize window=_window;
+ 
+ The backing variable for window will be called _window
+ 
+ This allows you to decouple the name of the property 
+ from the name of your variable. 
+ In fact, if you don't use @synthesize you don't need to have a backing variable at all.
+ Everyone has different reasons, this is one:
+ 
+ To avoid clashes with variable names 
+ and make it clear when I'm using a local variable
+ and when I'm using an instance variable.
+ 
+*/
+
+@synthesize window=_window;
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    // Override point for customization after application launch.
+    [self.window makeKeyAndVisible];
+    return YES;
+}
+
+- (void)applicationWillResignActive:(UIApplication *)application
+{
+    /*
+     Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+     Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+     */
+}
+
+- (void)applicationDidEnterBackground:(UIApplication *)application
+{
+    /*
+     Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
+     If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+     */
+}
+
+- (void)applicationWillEnterForeground:(UIApplication *)application
+{
+    /*
+     Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+     */
+}
+
+- (void)applicationDidBecomeActive:(UIApplication *)application
+{
+    /*
+     Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+     */
+}
+
+- (void)applicationWillTerminate:(UIApplication *)application
+{
+    /*
+     Called when the application is about to terminate.
+     Save data if appropriate.
+     See also applicationDidEnterBackground:.
+     */
+}
+
+- (void)dealloc
+{
+    [_window release];
+    [super dealloc];
+}
+
+@end
